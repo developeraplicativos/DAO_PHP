b7b265e18d14137aad59fdd8256814265c43d8b3
developeraplicativos <developer.aplicativos@gmail.com>
2017-06-20 22:02:33 -0300

    separei por pasta

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 spl_autoload_register(function($nomeClasse){
-$caminho = $nomeClasse.".php";
+$caminho = "class".DIRECTORY_SEPARATOR.$nomeClasse.".php";
     if(file_exists($caminho)){
       require_once("$caminho");
     }
